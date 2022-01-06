179216ef96cbd3d4fb5b0d62f97a7e24e22523d9
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-01-06 13:33:39 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+        'bgWhite':'#E5E5E5',
+        'offWhite':'#FBFBFB',
+        'borderColor':'rgba(115, 123, 125, 0.3)',
+        'paragraphColor':'#737B7D',
+      },
+
+    },
+  },
+  plugins: [],
+}
