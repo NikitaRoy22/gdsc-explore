672573a7b4c2205ce7fd1d44ceb103e11855700e
NikitaRoy22 <nikitaisonline0@gmail.com>
2022-01-10 12:15:20 +0530

    added footer

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import EventsLayout from '../components/events/EventsLayout'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <>
       <div id="events-section">
          <EventsLayout/>
+      </div>
+      <div id="footer-section">
+        <Footer/>
       </div>
     </>
    
