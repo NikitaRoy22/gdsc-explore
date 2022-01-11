764bac3892e0772f2ef379274b10248b16badabe
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-01-11 13:08:07 +0530

    Update Card.js

--- components/events/Card.js
+++ components/events/Card.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import navigateicon from "../../public/images/navigateicon.png";
+import navigateicon from "../../public/images/navigateIcon.png";
 import Link from "next/link";
 
 export default function Card({ event }) {
