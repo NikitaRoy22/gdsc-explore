57d761cf3f5dc3451d26548ce2426e6670e17f68
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-01-14 10:38:51 +0530

    Revert "Revert "Merge pull request #4 from NikitaRoy22/dev""
    This reverts commit 3074c32ae123ec30744b8049bd41448cfb113143.

--- components/Footer.js
+++ components/Footer.js
@@ -1,42 +1,21 @@
 import Image from 'next/image';
-import LogoSvg from '../public/logo.svg'
+import GDSCexplore from '../public/gdscexplore.svg'
 import Link from 'next/link';
 
 export default function Footer() {
     return(
         <>
-            <div className="py-14">
+            <div className="py-14 flex flex-col items-center">
                 <div className="flex flex-row justify-center">
                     <p>email us at </p>
                     <Link href={"/"}><p className="mr-1 px-1 text-indigo-600">example@gmail.com</p></Link>
                 </div>
-                <div className="flex flex-wrap justify-center mx-[4rem] md:mx-[19%] lg:mx-[15%] mt-[35px]">
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%] mb-[7px] py-8'>
-                        <Image src={LogoSvg} className="w-8 h-8"/>
-                        <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
-                        <div className='text-[#737B7D] text-[9.39px] text-center'>National Institute Of Technology Silchar</div>
-                    </div>
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%] mb-[7px] py-8'>
-                        <Image src={LogoSvg} className="w-8 h-8"/> 
-                        <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
-                        <div className='text-[#737B7D] text-[9.39px] text-center'>National Institute Of Technology Silchar</div>
-                    </div>
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%] mb-[7px] py-8'>
-                        <Image src={LogoSvg} className="w-8 h-8"/>
-                        <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
-                        <div className='text-[#737B7D] text-[9.39px] text-center'>National Institute Of Technology Silchar</div>
-                    </div>
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%] mb-[7px] py-8'>
-                        <Image src={LogoSvg} className="w-8 h-8"/>
-                        <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
-                        <div className='text-[#737B7D] text-[9.39px] text-center'>National Institute Of Technology Silchar</div>
-                    </div>
-                </div>
+                <div className='w-40 py-4 pt-8'><Image src={GDSCexplore} className="w-8 h-8"/></div>
                 <div className="flex flex-col justify-center items-center">
-                    <p className=" text-center mr-1 pb-2">Developers</p>
-                    <hr className="w-[2%]"></hr>
+                    <p className=" text-center text-[1.8rem] text-[#4F5356] font-bold mr-1 pb-2">GDSC EXPLORE</p>
+                    <hr className="w-[8%] pt-3"></hr>
                     <Link href={"/"} >
-                        <a className="pt-2">
+                        <a className="pt-1">
                             <p className="text-[#737B7D] mr-1">Website Developers</p>
                         </a>
                     </Link>
