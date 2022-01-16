3df5afb26158fd2cac4b720b696c6b8e6df77fdf
Vinayak Das <vinayakd909@gmail.com>
2022-01-16 13:11:31 +0530

    Add Bottom Margin To Logo

--- components/ourteam.js
+++ components/ourteam.js
@@ -22,22 +22,22 @@ export default function Ourteam(){
 
 
                 <div id="cards" className="flex flex-wrap justify-content mx-[4rem] md:mx-[19%] lg:mx-[15%] mt-[40px] mb-[10px]">
-                    <div className='flex flex-col w-[100%] px-0 sm:w-[50%] lg:w-[25%]'>
+                    <div className='flex flex-col w-[100%] px-0 mb-6 sm:w-[50%] lg:w-[25%]'>
                         <Image src={LogoSvg}/>
                         <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
                         <div className='text-[#737B7D] text-[9.39px] text-center'>ASSAM ENGINEERING COLLEGE</div>
                     </div>
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%]'>
+                    <div className='flex flex-col w-[100%] mb-6 sm:w-[50%] lg:w-[25%]'>
                         <Image src={LogoSvg}/>
                         <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
                         <div className='text-[#737B7D] text-[9.39px] text-center'>National Institute Of Technology Silchar</div>
                     </div>
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%]'>
+                    <div className='flex flex-col w-[100%] mb-6 sm:w-[50%] lg:w-[25%]'>
                         <Image src={LogoSvg}/>
                         <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
                         <div className='text-[#737B7D] text-[9.39px] text-center'>National Institute Of Technology Agartala</div>
                     </div>
-                    <div className='flex flex-col w-[100%] sm:w-[50%] lg:w-[25%]'>
+                    <div className='flex flex-col w-[100%] mb-5 sm:w-[50%] lg:w-[25%]'>
                         <Image src={LogoSvg}/>
                         <div className='text-[#737B7D] text-[14.5px] text-center mt-[10px]'>Google Developer Student Clubs</div>
                         <div className='text-[#737B7D] text-[9.39px] text-center'>Indian Institute Of Information Technology Guwahati</div>
